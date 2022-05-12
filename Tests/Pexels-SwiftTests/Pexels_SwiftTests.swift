d4dc77d10a6358ae9c22e9a89f1a9ca9de365b86
Lukas Pistrol <lukas@pistrol.com>
2022-05-12 15:29:19 +0200

    initial commit

--- Tests/Pexels-SwiftTests/Pexels_SwiftTests.swift
+++ Tests/Pexels-SwiftTests/Pexels_SwiftTests.swift
@@ -2,10 +2,14 @@ import XCTest
 @testable import Pexels_Swift
 
 final class Pexels_SwiftTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Pexels_Swift().text, "Hello, World!")
+
+
+    func testExample() async throws {
+        let pexels = PexelsSwift.shared
+        pexels.setAPIKey(apiKey)
+
+        let photos = await pexels.fetch()
+
+        print(photos.count)
     }
 }
