cb22b4cf8a67700e5cf4e80ed835ce10838d8b6e
Lukas Pistrol <lukas@pistrol.com>
2022-05-14 12:00:32 +0200

    removed unneeded enum

--- Sources/PexelsSwift/PexelsSwift+Extensions/PexelsSwift+API.swift
+++ Sources/PexelsSwift/PexelsSwift+Extensions/PexelsSwift+API.swift
@@ -9,12 +9,12 @@ import Foundation
 
 internal extension PexelsSwift {
     enum API {
-        static let searchPhotos: String = "https://api.pexels.com/v1/search"
-        static let curatedPhotos: String = "https://api.pexels.com/v1/curated"
         static let collections: String = "https://api.pexels.com/v1/collections"
+        static let searchPhotos: String = "https://api.pexels.com/v1/search"
+        static let searchVideos: String = "https://api.pexels.com/videos/search"
+        static let curatedPhotos: String = "https://api.pexels.com/v1/curated"
+        static let popularVideos: String = "https://api.pexels.com/videos/popular"
         static let photoByID: String = "https://api.pexels.com/v1/photos"
         static let videoByID: String = "https://api.pexels.com/videos/videos"
-        static let popularVideos: String = "https://api.pexels.com/videos/popular"
-        static let searchVideos: String = "https://api.pexels.com/videos/search"
     }
 }
